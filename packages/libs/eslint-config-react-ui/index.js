44637c2a940c563fc8cccc86eb3b4a9c668452e8
Jay Lee <jaylee.possible@gmail.com>
2023-03-18 00:46:09 +0900

    chore: clean up non intended packages

---
+++ packages/libs/eslint-config-react-ui/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "turbo",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+};
