66a7525a2d540323815949868fbe1ced846dfdaa
Jay Lee <jaylee.possible@gmail.com>
2023-03-20 04:13:03 +0900

    feat(modal): create modal component

---
+++ packages/ui/modal/stories/Modal.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import Modal from "../src/Modal";
+
+export default {
+  title: "Modal",
+  component: Modal,
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
